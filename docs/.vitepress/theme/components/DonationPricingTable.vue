fcffd4a6f617e18e987091b45cce8123863daaef
AzeTIIx <93607991+AzeTIIx@users.noreply.github.com>
2025-12-20 18:17:13 +0100

    Delete Stripe links

--- docs/.vitepress/theme/components/DonationPricingTable.vue
+++ docs/.vitepress/theme/components/DonationPricingTable.vue
@@ -6,7 +6,7 @@
       price: '$any',
       period: 'one-time',
       description: 'Support us with a one-time donation. Every donation, no matter the size, makes a difference.',
-      stripeLink: 'https://donate.stripe.com/5kA03P73O2vv2pqcMM',
+      stripeLink: '',
       donateButtonText: 'Donate once'
     },
     {
@@ -15,7 +15,7 @@
       price: '$50',
       period: 'per year',
       description: 'For frequent users of The Hacker Recipes who want the project to cover more ground as soon as possible.',
-      stripeLink: 'https://donate.stripe.com/8wM17Tbk41rre88001',
+      stripeLink: '',
       donateButtonText: 'Donate yearly'
     }
   ]
